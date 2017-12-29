6a29ac226645f05c9bc32a24e3740e2a659a40d3
Lucy Miller <Lucy.Miller@gmail.com>
2017-12-29 23:38:16 +0000

    Save Function with Validation

--- app/src/main/java/com/apress/gerber/diaryapplication/DiaryActivity.java
+++ app/src/main/java/com/apress/gerber/diaryapplication/DiaryActivity.java
@@ -1,13 +1,54 @@
 package com.apress.gerber.diaryapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class DiaryActivity extends AppCompatActivity {
+
+    private EditText mEtTitle;
+    private EditText mEtContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary);
+
+        mEtTitle = (EditText) findViewById(R.id.diarytitle);
+        mEtContext = (EditText) findViewById(R.id.diarycontent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_diary_new, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_diary_save:
+                saveDiary();
+
+                break;
+        }
+        return true;
+    }
+    private void saveDiary(){
+        Diary diary = new Diary(System.currentTimeMillis(), mEtTitle.getText().toString()
+                , mEtContext.getText().toString());
+
+        if(Utilities.saveDiary(this, diary)) {
+            Toast.makeText (getApplicationContext(), "Success!! Diary Entry Saved", Toast.LENGTH_SHORT).show();
+
+        }
+        else {
+            Toast.makeText(getApplicationContext(), "Not Saved, do you have enough space?", Toast.LENGTH_SHORT).show();
+        }
+        finish();
     }
 }
